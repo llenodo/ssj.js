4c853a287775f75befcc1bc19a97f2a6010e036b
Ryan O'Donnell <ryanjodonnell@gmail.com>
2012-07-14 18:26:39 -0700

    update docs w/ latest version

--- ssj.js
+++ ssj.js
@@ -14,6 +14,7 @@ if (typeof Object.create !== 'function') {
 
 (function($, window, document, undefined){
 	var SSJ = {
+
 		init: function(options, el) {
 			var self = this;
 
@@ -23,6 +24,8 @@ if (typeof Object.create !== 'function') {
 
 			this.currentStyleIndex = 0;
 			this.powerLevelStyleRanges = this.options.powerLevelStyleRanges;
+			this.immediate = this.options.immediate;
+			this.grow = this.options.grow;
 			this.styleAttrs = this.powerLevelStyleRanges[this.currentStyleIndex];
 
 			this.blurColor = this.styleAttrs.blurColor;
@@ -30,21 +33,27 @@ if (typeof Object.create !== 'function') {
 			this.blurRadiusMax = this.styleAttrs.blurRadiusMax;
 			this.blurRadiusMin = this.styleAttrs.blurRadiusMin;
 			this.incrementSize = this.styleAttrs.incrementSize;
+			this.color = this.styleAttrs.color;
 			
 
 			this.glowing = false;
 			this.animateGlowTimeout = this.options.animateGlowTimeout;
 			this.clickCount = 0;
 
+			if (this.immediate) {
+				this.animateGlow();
+			}
+
 			this.$el.on('click', $.proxy(this.handleClick, this));
 		},
+
 		handleClick: function(){
 			this.clickCount++;
 			this.updateStyleAttributes();
 
 			//grow if we're already glowing - wont grow on the first click
 			if (this.glowing) {
-				this.grow();
+				this.growMe();
 			}
 
 			//start glowing if needed
@@ -52,6 +61,7 @@ if (typeof Object.create !== 'function') {
 				this.animateGlow();
 			}
 		},
+
 		updateStyleAttributes: function() {
 
 			//if we've reached the max clicks for this style and there is another style
@@ -63,13 +73,10 @@ if (typeof Object.create !== 'function') {
 
 				for (style in this.styleAttrs) {
 					this[style] = this.styleAttrs[style];
-
-					if (style === 'color') {
-						this.$el.css('color', this.color)
-					}
 				}
 			}
 		},
+
 		animateGlow: function(){
 			var self = this;
 
@@ -79,8 +86,11 @@ if (typeof Object.create !== 'function') {
 			//toggle between two different blur radii		        
 			this.blurRadius = (this.blurRadius === this.blurRadiusMax ? this.blurRadiusMin : this.blurRadiusMax);
 		        
-	        //set the text shadow
-	        this.$el.css('text-shadow', "0px 0px " + this.blurRadius + "px " + this.blurColor);
+	        //set the text shadow and color
+	        this.$el.css({
+	        	'text-shadow': "0px 0px " + this.blurRadius + "px " + this.blurColor,
+	        	color: this.color
+	        });
 
 	        //animate every specified time period
 	        setTimeout(function(){
@@ -89,14 +99,16 @@ if (typeof Object.create !== 'function') {
 
 	        }, self.animateGlowTimeout);
 		},
-		grow: function(){
-			var self = this;
 
-			this.$el.animate({
-				'font-size': ('+=' + self.incrementSize)
-			});
+		growMe: function(){
+			if (this.grow) {
+				var self = this;
+
+				this.$el.animate({
+					'font-size': ('+=' + self.incrementSize)
+				});
+			}
 		}
-
 	}
 
 	$.fn.ssj = function(options){
@@ -116,24 +128,20 @@ if (typeof Object.create !== 'function') {
 			
 		});	
 	}
-	
+
 	$.fn.ssj.defaults = {
 	    animateGlowTimeout: 180,  	//milliseconds between each glow animation
+	    immediate: true,
+	    grow: false,
 	    powerLevelStyleRanges: [
-	    	{
-	    		maxClicks: 5,	//max clicks before moving to next style range
-	    		blurRadiusMax: 16,  //max text blur 
-	    		blurRadiusMin: 12,	//min text blur
-	    		blurColor: '#3D85C6', //blueish color
-	    		incrementSize: 6	//amount you want to increment the size on each click
-	    	},
 	    	//SSJ!!
 	    	{				
 	    		incrementSize: 20,
 	    		blurRadiusMin: 18,
 	    		blurRadiusMax: 27,
 	    		blurColor: 'yellow',
-	    		color: 'orange'
+	    		color: 'orange',
+	    		incrementSize: 10
 	    	}
 	    ]
 	};
